89b871d37e1cb3f91a14f19fd499fac92c276b72
Samuel Wachira <187swachira@gmail.com>
2021-05-07 16:13:00 -0400

    re-added dog_proj

---
+++ dog_project/lib/dog.rb
@@ -0,0 +1,45 @@
+class Dog
+
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age 
+    end
+
+    def age=(age)
+        @age = age
+    end
+
+    def bark
+        if @age > 3
+            return @bark.upcase
+        elsif @age <= 3
+            return @bark.downcase
+        else
+            nil
+        end  
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(food_item)
+        @favorite_foods.map(&:downcase).include?(food_item.downcase)
+    end
+    
+end
